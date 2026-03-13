e34e3c37907efb1e36c14224dd061451ae21e012
Muhammed Salih Özer <muhammedsalih.ozer@semerkand.tv>
2026-03-13 03:29:11 +0300

    fix: configure api cors for admin domain

--- umre-portal/apps/api/src/main.ts
+++ umre-portal/apps/api/src/main.ts
@@ -6,7 +6,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'https://admin.kasriroyal.com',
+            'https://kasriroyal.com',
+            'https://www.kasriroyal.com'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
